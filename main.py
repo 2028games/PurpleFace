727efc02cee65b62ba8ff9f1bc23685924d35a24
Thanasis Katsios <thkatsios@gmail.com>
2015-03-14 15:11:17 +0200

    test commit

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def main():
     game.start(game.screen)  # start loop
 
 if __name__ == "__main__":
-    #cProfile.run("main()") #start!
-    main()
+    #cProfile.run("main()")
+    main() #start!
